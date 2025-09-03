a59fe07771f0df7d4742d945eaaab5f8f0b04702
keshav9761 <keshavtomar53@gmail.com>
2025-09-03 22:23:55 +0530

    upadate test done

--- src/components/AnimatedTypography.jsx
+++ src/components/AnimatedTypography.jsx
@@ -37,7 +37,7 @@ const AnimatedTypography = () => {
                     }
                 }}
             >
-                Keshav Tomars
+                Keshav Tomar
             </Typography>
         </motion.div>
     );
